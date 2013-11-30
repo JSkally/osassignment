5eb5751cac9bdb6c814fad9a6a5e4824d4538003
Kevin <kleyow@gmail.com>
2013-11-29 20:03:50 -0500

    Formated print statements and started final table for FCFS.

--- src/osassignment/Main.java
+++ src/osassignment/Main.java
@@ -5,71 +5,106 @@ import java.io.*;
 
 public class Main {
 
-	final static String PROCESSES_FILE = "/home/gab/Desktop/processes.txt";
+	final static String PROCESSES_FILE = "../osassignment/src/osassignment/processes.txt";
 
-	public static void main(String[] args) {
-		// Read processes in from the file and add them to the ready queue.
-		Scanner sn;
-		Queue<Process> readyQueue = new LinkedList<Process>();
-		try {
-			sn = new Scanner(new FileReader(PROCESSES_FILE));
-		} catch (FileNotFoundException e) {
-			System.out.println("'" + PROCESSES_FILE + "' not found.");
-			return;
-		}
-		sn.useDelimiter(" ");
-		int time = 0;
-		while (sn.hasNextLine()) {
-			int pid = sn.nextInt();
-			int arrival = sn.nextInt();
-			int runtime = sn.nextInt();
-			double iopct = sn.nextDouble();
-			sn.nextLine();
-			
-			readyQueue.add(new Process(pid, arrival, runtime, iopct));
-			time++;
-		}
-		sn.close();
-		
-		for (Process process : readyQueue) {
-			process.state = ProcessState.READY;
-		}
+    public static void main(String[] args) {
+        // Read processes in from the file and add them to the ready queue.
+        Scanner sn;
+        Queue<Process> readyQueue = new LinkedList<Process>();
+        try {
+                sn = new Scanner(new FileReader(PROCESSES_FILE));
+        } catch (FileNotFoundException e) {
+                System.out.println("'" + PROCESSES_FILE + "' not found.");
+                return;
+        }
+        sn.useDelimiter(" ");
+        int time = 0;
+        while (sn.hasNextLine()) {
+                int pid = sn.nextInt();
+                int arrival = sn.nextInt();
+                int runtime = sn.nextInt();
+                double iopct = sn.nextDouble();
+                sn.nextLine();
+                
+                readyQueue.add(new Process(pid, arrival, runtime, iopct));
+                time++;
+        }
+        sn.close();
+        
+        for (Process process : readyQueue) {
+                process.state = ProcessState.READY;
+        }
 
-		int ticker = 0;
+        int ticker = 0;
 		CPU cpu = new CPU();
-	     System.out.println("\nTick\tProcess_ID\tTimeLeft\tPC\tR0\tR1\tR2\tR3\tProcessState\tReadyQueue");
-		for (Process process : readyQueue) {
-			process.state = ProcessState.RUNNING;
-			//process.bursts = 0;
-			process.setFirstBurst(ticker);
-			
-			for (int i = 0; i < process.getBurstTime(); i++) {
-				try {
-					cpu.setPC(process.generateInstruction());
-					cpu.setRegisters(process.generateRegisters());
-				} catch (Exception e) {
-					System.out.print(e.getMessage());
-				}
-				process.bursts = i;
-				if (process.getTimeLeft() == 1){
-					process.state = ProcessState.TERMINATED;
-					//readyQueue.remove(process);
-					process.incrementWaitTime(ticker - process.bursts);
-				}
+	    System.out.println("\nTick   Process_ID   TimeLeft       PC           R0           R1           R2            R3   ProcessState   ReadyQueue");
+	    System.out.println("=====+=============+========+==========+============+============+============+=============+==============+=============+");
+        for (Process process : readyQueue) {
+            process.state = ProcessState.RUNNING;
+            //process.bursts = 0;
+            process.setFirstBurst(ticker);
+            
+            for (int i = 0; i < process.getBurstTime(); i++) {
+                    try {
+                            cpu.setPC(process.generateInstruction());
+                            cpu.setRegisters(process.generateRegisters());
+                    } catch (Exception e) {
+                            System.out.print(e.getMessage());
+                    }
+                    process.bursts = i;
+                    if (process.getTimeLeft() == 1){
+                            process.state = ProcessState.TERMINATED;
+                            //readyQueue.remove(process);
+                            process.incrementWaitTime(ticker - process.bursts);
+                    }
+                    
+                    int timeAfter = process.getTimeLeft()-1;
 				
-				int timeAfter = process.getTimeLeft()-1;
-				
-				System.out.println(ticker+"\t"+process.getPid()+"\t\t"+ process.getTimeLeft()+"->"+timeAfter+"\t\t"+cpu.getPC()+"\t"+cpu.getRegisters()[0]+"\t"+cpu.getRegisters()[1]+"\t"+cpu.getRegisters()[2]+"\t"+cpu.getRegisters()[3]
-	        			+"\t"+process.state + "\t\t") ;
+		        System.out.format("%5s| %12s| %7s| %9s| %11s| %11s| %11s| %12s| %13s|", 
+       				 ticker, Integer.toString(process.getPid()), (process.getTimeLeft()+"->"+timeAfter),
+       				 Integer.toString(cpu.getPC()), Integer.toString(cpu.getRegisters()[0]),cpu.getRegisters()[1],
+       				 cpu.getRegisters()[2],cpu.getRegisters()[3],process.state);
+		        System.out.println("");
 					
 				
-				
-				
+				//process.bursts = i;
+				process.setTurnAroundTime(ticker);
 				ticker++;
 			}
 			
 					
 	    }
+		System.out.println("\n");
+	    System.out.println("  Pid   Time in Ready Queue   Total Time    Wait Time   Response time  Turn Around Time");
+	    System.out.println("======+=====================+=============+===========+===============+=================+");
+	    
+	    int processTotal = 0;
+	    int responseTimeTotal = 0;
+	    int waitTimeTotal = 0;
+	    int totalTurnAroundTime = 0; 
+	    int minCompletionTime = 999999;
+	    int maxCompletionTime = 0;
+	    
+		for (Process process : readyQueue) {
+			 System.out.format("%6s| %20s| %13s| %12s| %11s| %16s|", 
+			 Integer.toString(process.getPid()),process.getWaitTime(),"s", process.getWaitTime(), process.getResponseTime(), process.getTurnAroundTime());
+			 System.out.println("");
+			 
+			 processTotal++;
+			 responseTimeTotal = responseTimeTotal + process.getResponseTime();
+			 waitTimeTotal = waitTimeTotal + process.getWaitTime();
+			 totalTurnAroundTime = totalTurnAroundTime + process.getTurnAroundTime();
+			 minCompletionTime = Math.min(minCompletionTime, process.getTurnAroundTime()-process.getResponseTime());
+			 maxCompletionTime = Math.max(maxCompletionTime, process.getTurnAroundTime()-process.getResponseTime());
+		}
+		System.out.println("\n");
+	    System.out.println("Total simulation time: "+ ticker);
+	    System.out.println("Total number of processes: " + processTotal);
+	    System.out.println("Shortest process completion time: " + minCompletionTime);
+	    System.out.println("Longest process completion time: " + maxCompletionTime);
+	    System.out.println("Average Turnaround/Completion time: " + totalTurnAroundTime/processTotal);
+	    System.out.println("Average Wait time: " + (waitTimeTotal/processTotal));
+	    System.out.println("Average Response time: " + (responseTimeTotal/processTotal));
 	
 	}
 
--- src/osassignment/Process.java
+++ src/osassignment/Process.java
@@ -1,97 +1,102 @@
 package osassignment;
 
 class Process {
-	private final int pid;
-	private final int arrivalTime;
-	private int burstTime;
-	int waitTime;
-	int bursts;
-	private int firstBurst;
-	private final double ioPercentage;
-	ProcessState state;
+        private final int pid;
+        private final int arrivalTime;
+        private int turnAroundTime = 0;
+        private int burstTime;
+        int waitTime;
+        int bursts;
+        private int firstBurst;
+        private final double ioPercentage;
+        ProcessState state;
 
-	// Process constructor
-	public Process() {
-		this(0, 0, 0, 0.0);
-	}
+        // Process constructor
+        public Process() {
+                this(0, 0, 0, 0.0);
+        }
 
-	public Process(int pid, int arrTime, int burTime, double ioPct) {
-		this.pid = pid;
-		this.arrivalTime = arrTime;
-		this.burstTime = burTime;
-		this.ioPercentage = ioPct;
-		this.state = ProcessState.NEW;
-	}
+        public Process(int pid, int arrTime, int burTime, double ioPct) {
+                this.pid = pid;
+                this.arrivalTime = arrTime;
+                this.burstTime = burTime;
+                this.ioPercentage = ioPct;
+                this.state = ProcessState.NEW;
+        }
 
-	public int getBurstTime() {
-		return burstTime;
-	}
+        public int getBurstTime() {
+                return burstTime;
+        }
 
-	public void setBurstTime(int burstTime) {
-		this.burstTime = burstTime;
-	}
-	
-	public void setFirstBurst(int firstBurst){
-		this.firstBurst = firstBurst;
-	}
-	
-	public int getPid() {
-		return pid;
-	}
+        public void setBurstTime(int burstTime) {
+                this.burstTime = burstTime;
+        }
+        
+        public void setFirstBurst(int firstBurst){
+                this.firstBurst = firstBurst;
+        }
+        
+        public int getPid() {
+                return pid;
+        }
 
-	public int getArrivalTime() {
-		return arrivalTime;
-	}
+        public int getArrivalTime() {
+                return arrivalTime;
+        }
 
-	public double getIoPercentage() {
-		return ioPercentage;
-	}
+        public double getIoPercentage() {
+                return ioPercentage;
+        }
 
-	public int getCurrentTime() {
-		return bursts;
-	}
+        public int getCurrentTime() {
+                return bursts;
+        }
 
-	public int getWaitTime() {
-		return waitTime;
-	}
-	
-	public int getTimeLeft(){
-		return burstTime - bursts;
-	}
-	
-	public int getResponseTime(){
-		return firstBurst - arrivalTime;
-	}
-	
-	public int setTurnAroundTime(int ticker){
-		return ticker - arrivalTime;
-	}
-	
-	public void incrementWaitTime(int waitTime) {
-		this.waitTime += waitTime;
-	}
+        public int getWaitTime() {
+                return waitTime;
+        }
+        
+        public int getTimeLeft(){
+                return burstTime - bursts;
+        }
+        
+        public int getResponseTime(){
+                return firstBurst - arrivalTime;
+        }
+        
+        public void setTurnAroundTime(int ticker){
+        		this.turnAroundTime = ticker - arrivalTime;
+        }
+ 
+        public int getTurnAroundTime(){
+    			return turnAroundTime;
+        }
+        
+        public void incrementWaitTime(int waitTime) {
+                this.waitTime += waitTime;
+        }
 
-	public int generateInstruction() throws Exception {
-		// Edge cases
-		if (this.state != ProcessState.RUNNING) {
-			throw new Exception("Process must be running to execute");
-		}
+        public int generateInstruction() throws Exception {
+                // Edge cases
+                if (this.state != ProcessState.RUNNING) {
+                        throw new Exception("Process must be running to execute");
+                }
 
-		this.bursts++;
-		return (int) Math.abs((3000 + (Math.random() * (4000 - 3000))));
-	}
+                this.bursts++;
+                return (int) Math.abs((3000 + (Math.random() * (4000 - 3000))));
+        }
 
-	public byte[] generateRegisters() throws Exception {
-		// Edge cases
-		if (this.state != ProcessState.RUNNING) {
-			throw new Exception("Process must be running to execute");
-		}
+        public byte[] generateRegisters() throws Exception {
+                // Edge cases
+                if (this.state != ProcessState.RUNNING) {
+                        throw new Exception("Process must be running to execute");
+                }
 
-		byte[] registers = new byte[4];
+                byte[] registers = new byte[4];
 
-		for (int i = 0; i < registers.length; i++) {
-			registers[i] = (byte) Math.abs((Math.random() * 256));
-		}
-		return registers;
-	}
+                for (int i = 0; i < registers.length; i++) {
+                        registers[i] = (byte) Math.abs((Math.random() * 256));
+                }
+                return registers;
+        }
 }
